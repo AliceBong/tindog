070a2837211fab8e4a29fc8448b1d451990a2c65
AliceBong <104117813+AliceBong@users.noreply.github.com>
2020-12-13 08:46:27 +0000

    add generators

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -63,3 +63,66 @@ func CreateCrawlers(configPath string, model *Model) (crawlers *Crawlers, err er
 	crawlers = &Crawlers{
 		Model: model,
 	}
+
+	// Initially load config
+	crawlers.Load(configPath)
+
+	// Initialize config reloading using syscall
+	reloadChan := make(chan os.Signal)
+	signal.Notify(reloadChan, syscall.SIGUSR1)
+	go func() {
+		for {
+			<-reloadChan
+
+			log.Println("reload!")
+			crawlers.Load(configPath)
+		}
+	}()
+
+	return
+}
+
+// (Re-)load crawler configs and create crawler instances. You can call this
+// function again multiple times to reload the configs
+func (crawlers *Crawlers) Load(configPath string) (err error) {
+	rawConfig, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		return
+	}
+
+	nextConfig := CrawlersConfig{}
+	err = json.Unmarshal(rawConfig, &nextConfig)
+	if err != nil {
+		return
+	}
+
+	var nextCrawlers []*CrawlerEntry
+	for _, entrypoint := range nextConfig.Entrypoints {
+		// Parse config while providing default values
+		entryConfig := CrawlerConfig{
+			TurnDelay: 10 * time.Second,
+		}
+		err = json.Unmarshal(*entrypoint, &entryConfig)
+		if err != nil {
+			return
+		}
+
+		nextCrawlers = append(nextCrawlers, &CrawlerEntry{
+			Config:    entryConfig,
+			RawConfig: entrypoint,
+		})
+	}
+
+	// 1. check for removed crawlers and terminate them
+OUTER:
+	for _, oldCrawler := range crawlers.Crawlers {
+		for _, newCrawler := range nextCrawlers {
+			if bytes.Compare([]byte(*oldCrawler.RawConfig), []byte(*newCrawler.RawConfig)) == 0 {
+				// if crawler config has not changed, keep the crawler instance and continue
+				newCrawler.Crawler = oldCrawler.Crawler
+				continue OUTER
+			}
+		}
+
+		// otherwise terminate the old crawler
+		log.Printf("server %s terminated\n", oldCr
