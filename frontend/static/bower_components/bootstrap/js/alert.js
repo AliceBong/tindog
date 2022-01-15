0589c2263015bde8d1f4a80dfc6d4c90a0888c31
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-01-15 06:55:45 +0000

    colorpicker and kernel

---
+++ frontend/static/bower_components/bootstrap/js/alert.js
@@ -0,0 +1,21 @@
+/* ========================================================================
+ * Bootstrap: alert.js v3.3.1
+ * http://getbootstrap.com/javascript/#alerts
+ * ========================================================================
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
+
+
++function ($) {
+  'use strict';
+
+  // ALERT CLASS DEFINITION
+  // ======================
+
+  var dismiss = '[data-dismiss="alert"]'
+  var Alert   = function (el) {
+    $(el).on('click', dismiss, this.close)
+  }
+
+  Alert.VERSION = 
