01fc9c6ae5b5f3e7ea6f20618c695e79d57bf5dc
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-01-16 18:26:31 +0000

    All the recent Reg Z fixes

---
+++ frontend/static/bower_components/bootstrap/js/button.js
@@ -0,0 +1,17 @@
+
+/* ========================================================================
+ * Bootstrap: button.js v3.3.1
+ * http://getbootstrap.com/javascript/#buttons
+ * ========================================================================
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
+
+
++function ($) {
+  'use strict';
+
+  // BUTTON PUBLIC CLASS DEFINITION
+  // ==============================
+
+  var Button = function (element, options) {
