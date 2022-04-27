0a01c6b0134fe9910e94a00b576c964f56a37051
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-04-27 13:31:35 +0000

    * Code cleanup

---
+++ frontend/static/bower_components/jquery/dist/jquery.js
@@ -0,0 +1,18 @@
+/*!
+ * jQuery JavaScript Library v2.1.3
+ * http://jquery.com/
+ *
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ *
+ * Copyright 2005, 2014 jQuery Foundation, Inc. and other contributors
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: 2014-12-18T15:11Z
+ */
+
+(function( global, factory ) {
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+		// For CommonJS and CommonJS-like environments where a proper `wi
