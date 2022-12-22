f57bb1bdbfa67b6bb11459fdc3ad9e6cc3d35b3e
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-12-22 11:11:28 +0000

    Gitflow-Develop

---
+++ frontend/static/bower_components/jquery/src/attributes/prop.js
@@ -0,0 +1,16 @@
+define([
+	"../core",
+	"../core/access",
+	"./support"
+], function( jQuery, access, support ) {
+
+var rfocusable = /^(?:input|select|textarea|button)$/i;
+
+jQuery.fn.extend({
+	prop: function( name, value ) {
+		return access( this, jQuery.prop, name, value, arguments.length > 1 );
+	},
+
+	removeProp: function( name ) {
+		return this.each(function() {
+			delete this[ jQuery.propF
