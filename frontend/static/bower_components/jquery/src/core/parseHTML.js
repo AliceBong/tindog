91d4e7b3e9f253a003aa309b6a7c73374a88caae
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-12-22 22:48:56 +0000

    make it work again

---
+++ frontend/static/bower_components/jquery/src/core/parseHTML.js
@@ -0,0 +1,9 @@
+define([
+	"../core",
+	"./var/rsingleTag",
+	"../manipulation" // buildFragment
+], function( jQuery, rsingleTag ) {
+
+// data: string of html
+// context (optional): If specified, the fragment will be created in this context, defaults to document
+// k
