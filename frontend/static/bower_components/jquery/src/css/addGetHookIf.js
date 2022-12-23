fcd01771e22a50f985be2b2061e860afcd16b5c6
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-12-23 20:29:54 +0000

    asdfadsf

---
+++ frontend/static/bower_components/jquery/src/css/addGetHookIf.js
@@ -0,0 +1,8 @@
+define(function() {
+
+function addGetHookIf( conditionFn, hookFn ) {
+	// Define the hook, we'll check on the first run if it's really needed.
+	return {
+		get: function() {
+			if ( conditionFn() ) {
+				// Hook not needed (or it's not
