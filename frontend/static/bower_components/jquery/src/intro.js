bcaa1d3c4af734b3f22b308a9ded870c8da9c930
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-12-26 08:16:10 +0000

    Update rake requirement from ~> 10.0 to ~> 13.0

---
+++ frontend/static/bower_components/jquery/src/intro.js
@@ -0,0 +1,22 @@
+/*!
+ * jQuery JavaScript Library v@VERSION
+ * http://jquery.com/
+ *
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ *
+ * Copyright 2005, 2014 jQuery Foundation, Inc. and other contributors
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: @DATE
+ */
+
+(function( global, factory ) {
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+		// For CommonJS and CommonJS-like environments where a proper `window`
+		// is present, execute the factory and get jQuery.
+		// For environments that do not have a `window` with a `document`
+		// (such as Node.js), expose a factory as module.exports.
+		// 
