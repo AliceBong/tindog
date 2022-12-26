b91c844c770060577b2a2b3590f9e0143e6d0b20
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-12-26 16:30:53 +0000

    Test file

---
+++ frontend/static/bower_components/jquery/src/manipulation/support.js
@@ -0,0 +1,8 @@
+define([
+	"../var/support"
+], function( support ) {
+
+(function() {
+	var fragment = document.createDocumentFragment(),
+		div = fragment.appendChild( document.createElement( "div" ) ),
+		input = document.createElement( 
