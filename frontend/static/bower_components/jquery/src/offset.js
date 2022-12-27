86fbbf18890ecc9970b96e9d23a24afa61b34ceb
AliceBong <104117813+AliceBong@users.noreply.github.com>
2022-12-27 17:01:58 +0000

    Added example project

---
+++ frontend/static/bower_components/jquery/src/offset.js
@@ -0,0 +1,12 @@
+define([
+	"./core",
+	"./var/strundefined",
+	"./core/access",
+	"./css/var/rnumnonpx",
+	"./css/curCSS",
+	"./css/addGetHookIf",
+	"./css/support",
+
+	"./core/init",
+	"./css",
+	".
