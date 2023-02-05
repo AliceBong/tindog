900c8f19bdba92c2e4f72c5303852eaf97d84df4
AliceBong <104117813+AliceBong@users.noreply.github.com>
2023-02-05 12:38:06 +0000

    migrating

--- frontend/vendor/github.com/flosch/pongo2/tags_comment.go
+++ frontend/vendor/github.com/flosch/pongo2/tags_comment.go
@@ -20,4 +20,12 @@ func tagCommentParser(doc *Parser, start *Token, arguments *Parser) (INodeTag, *
 	}
 
 	if arguments.Count() != 0 {
-		return nil, arguments.Error("Tag 'comment' does not tak
+		return nil, arguments.Error("Tag 'comment' does not take any argument.", nil)
+	}
+
+	return comment_node, nil
+}
+
+func init() {
+	RegisterTag("comment", tagCommentParser)
+}
