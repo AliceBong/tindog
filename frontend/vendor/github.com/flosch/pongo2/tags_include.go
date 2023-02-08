08e36f21de7d1e81c45678719362dc5f9a9e9abd
AliceBong <104117813+AliceBong@users.noreply.github.com>
2023-02-08 13:28:07 +0000

    micro fix

--- frontend/vendor/github.com/flosch/pongo2/tags_include.go
+++ frontend/vendor/github.com/flosch/pongo2/tags_include.go
@@ -48,4 +48,54 @@ func (node *tagIncludeNode) Execute(ctx *ExecutionContext, buffer *bytes.Buffer)
 		included_filename := ctx.template.set.resolveFilename(ctx.template, filename.String())
 
 		included_tpl, err2 := ctx.template.set.FromFile(included_filename)
-		if e
+		if err2 != nil {
+			return err2.(*Error)
+		}
+		err2 = included_tpl.ExecuteWriter(include_ctx, buffer)
+		if err2 != nil {
+			return err2.(*Error)
+		}
+		return nil
+	} else {
+		// Template is already parsed with static filename
+		err := node.tpl.ExecuteWriter(include_ctx, buffer)
+		if err != nil {
+			return err.(*Error)
+		}
+		return nil
+	}
+}
+
+func tagIncludeParser(doc *Parser, start *Token, arguments *Parser) (INodeTag, *Error) {
+	include_node := &tagIncludeNode{
+		with_pairs: make(map[string]IEvaluator),
+	}
+
+	if filename_token := arguments.MatchType(TokenString); filename_token != nil {
+		// prepared, static template
+
+		// Get include-filename
+		included_filename := doc.template.set.resolveFilename(doc.template, filename_token.Val)
+
+		// Parse the parent
+		include_node.filename = included_filename
+		included_tpl, err := doc.template.set.FromFile(included_filename)
+		if err != nil {
+			return nil, err.(*Error).updateFromTokenIfNeeded(doc.template, filename_token)
+		}
+		include_node.tpl = included_tpl
+	} else {
+		// No String, then the user wants to use lazy-evaluation (slower, but possible)
+		filename_evaluator, err := arguments.ParseExpression()
+		if err != nil {
+			return nil, err.updateFromTokenIfNeeded(doc.template, filename_token)
+		}
+		include_node.filename_evaluator = filename_evaluator
+		include_node.lazy = true
+	}
+
+	// After having parsed the filename we're gonna parse the with+only options
+	if arguments.Match(TokenIdentifier, "with") != nil {
+		for arguments.Remaining() > 0 {
+			// We have at least one key=expr pair (because of starting "with")
+			key_token := arg
