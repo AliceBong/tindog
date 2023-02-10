184a8a82d84fd566dbd2330cec45926cbac204b6
AliceBong <104117813+AliceBong@users.noreply.github.com>
2023-02-10 08:45:07 +0000

    rt2x00: fix stop queue

--- frontend/vendor/github.com/flosch/pongo2/tags_lorem.go
+++ frontend/vendor/github.com/flosch/pongo2/tags_lorem.go
@@ -27,4 +27,75 @@ func (node *tagLoremNode) Execute(ctx *ExecutionContext, buffer *bytes.Buffer) *
 				if i > 0 {
 					buffer.WriteString("\n")
 				}
-				par := tagLoremPara
+				par := tagLoremParagraphs[rand.Intn(len(tagLoremParagraphs))]
+				buffer.WriteString(par)
+			}
+		} else {
+			for i := 0; i < node.count; i++ {
+				if i > 0 {
+					buffer.WriteString("\n")
+				}
+				par := tagLoremParagraphs[i%len(tagLoremParagraphs)]
+				buffer.WriteString(par)
+			}
+		}
+	case "w":
+		if node.random {
+			for i := 0; i < node.count; i++ {
+				if i > 0 {
+					buffer.WriteString(" ")
+				}
+				word := tagLoremWords[rand.Intn(len(tagLoremWords))]
+				buffer.WriteString(word)
+			}
+		} else {
+			for i := 0; i < node.count; i++ {
+				if i > 0 {
+					buffer.WriteString(" ")
+				}
+				word := tagLoremWords[i%len(tagLoremWords)]
+				buffer.WriteString(word)
+			}
+		}
+	case "p":
+		if node.random {
+			for i := 0; i < node.count; i++ {
+				if i > 0 {
+					buffer.WriteString("\n")
+				}
+				buffer.WriteString("<p>")
+				par := tagLoremParagraphs[rand.Intn(len(tagLoremParagraphs))]
+				buffer.WriteString(par)
+				buffer.WriteString("</p>")
+			}
+		} else {
+			for i := 0; i < node.count; i++ {
+				if i > 0 {
+					buffer.WriteString("\n")
+				}
+				buffer.WriteString("<p>")
+				par := tagLoremParagraphs[i%len(tagLoremParagraphs)]
+				buffer.WriteString(par)
+				buffer.WriteString("</p>")
+
+			}
+		}
+	default:
+		panic("unsupported method")
+	}
+
+	return nil
+}
+
+func tagLoremParser(doc *Parser, start *Token, arguments *Parser) (INodeTag, *Error) {
+	lorem_node := &tagLoremNode{
+		position: start,
+		count:    1,
+		method:   "b",
+	}
+
+	if count_token := arguments.MatchType(TokenNumber); count_token != nil {
+		lorem_node.count = AsValue(count_token.Val).Integer()
+	}
+
+	if method_token := arg
