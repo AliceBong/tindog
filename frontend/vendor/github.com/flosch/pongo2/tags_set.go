35cc989f447f5fdd6e1b6d59acdf29d90e3d367c
AliceBong <104117813+AliceBong@users.noreply.github.com>
2023-02-12 13:26:19 +0000

    Delete .Rapp.history

--- frontend/vendor/github.com/flosch/pongo2/tags_set.go
+++ frontend/vendor/github.com/flosch/pongo2/tags_set.go
@@ -20,3 +20,33 @@ func (node *tagSetNode) Execute(ctx *ExecutionContext, buffer *bytes.Buffer) *Er
 
 func tagSetParser(doc *Parser, start *Token, arguments *Parser) (INodeTag, *Error) {
 	node := &tagSetNode{}
+
+	// Parse variable name
+	typeToken := arguments.MatchType(TokenIdentifier)
+	if typeToken == nil {
+		return nil, arguments.Error("Expected an identifier.", nil)
+	}
+	node.name = typeToken.Val
+
+	if arguments.Match(TokenSymbol, "=") == nil {
+		return nil, arguments.Error("Expected '='.", nil)
+	}
+
+	// Variable expression
+	keyExpression, err := arguments.ParseExpression()
+	if err != nil {
+		return nil, err
+	}
+	node.expression = keyExpression
+
+	// Remaining arguments
+	if arguments.Remaining() > 0 {
+		return nil, arguments.Error("Malformed 'set'-tag arguments.", nil)
+	}
+
+	return node, nil
+}
+
+func init() {
+	RegisterTag("set", tagSetParser)
+}
