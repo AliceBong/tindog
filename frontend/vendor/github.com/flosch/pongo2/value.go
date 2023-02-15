1b484264ab88e4aafa9833c715d586a03d6d42d7
AliceBong <104117813+AliceBong@users.noreply.github.com>
2023-02-15 17:52:54 +0000

    relinked

--- frontend/vendor/github.com/flosch/pongo2/value.go
+++ frontend/vendor/github.com/flosch/pongo2/value.go
@@ -12,4 +12,58 @@ type Value struct {
 	safe bool // used to indicate whether a Value needs explicit escaping in the template
 }
 
-// Converts any given value to a pongo2.Val
+// Converts any given value to a pongo2.Value
+// Usually being used within own functions passed to a template
+// through a Context or within filter functions.
+//
+// Example:
+//     AsValue("my string")
+func AsValue(i interface{}) *Value {
+	return &Value{
+		val: reflect.ValueOf(i),
+	}
+}
+
+// Like AsValue, but does not apply the 'escape' filter.
+func AsSafeValue(i interface{}) *Value {
+	return &Value{
+		val:  reflect.ValueOf(i),
+		safe: true,
+	}
+}
+
+func (v *Value) getResolvedValue() reflect.Value {
+	if v.val.IsValid() && v.val.Kind() == reflect.Ptr {
+		return v.val.Elem()
+	}
+	return v.val
+}
+
+// Checks whether the underlying value is a string
+func (v *Value) IsString() bool {
+	return v.getResolvedValue().Kind() == reflect.String
+}
+
+// Checks whether the underlying value is a bool
+func (v *Value) IsBool() bool {
+	return v.getResolvedValue().Kind() == reflect.Bool
+}
+
+// Checks whether the underlying value is a float
+func (v *Value) IsFloat() bool {
+	return v.getResolvedValue().Kind() == reflect.Float32 ||
+		v.getResolvedValue().Kind() == reflect.Float64
+}
+
+// Checks whether the underlying value is an integer
+func (v *Value) IsInteger() bool {
+	return v.getResolvedValue().Kind() == reflect.Int ||
+		v.getResolvedValue().Kind() == reflect.Int8 ||
+		v.getResolvedValue().Kind() == reflect.Int16 ||
+		v.getResolvedValue().Kind() == reflect.Int32 ||
+		v.getResolvedValue().Kind() == reflect.Int64 ||
+		v.getResolvedValue().Kind() == reflect.Uint ||
+		v.getResolvedValue().Kind() == reflect.Uint8 ||
+		v.getResolvedValue().Kind() == reflect.Uint16 ||
+		v.getResolvedValue().Kind() == reflect.Uint32 ||
+		v.getResolvedValu
